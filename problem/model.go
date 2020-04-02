314af2b4d98b16482ab5d45eb817d5c84941b6fe
higashi000 <higashi.ict@gmail.com>
2020-04-02 22:48:35 +0900

    Change json member name

--- problem/model.go
+++ problem/model.go
@@ -1,9 +1,14 @@
 package problem
 
 type SetList struct {
-	ID       string    `json:"_id" bson:"_id"`
+	ID       string    `json:"id" bson:"id"`
 	Title    string    `json:"title" bson:"title"`
 	Problems []Problem `json:"problems" bson:"problems"`
+}
+
+type TitleID struct {
+	ID    string `json:"_id" bson:"_id"`
+	Title string `json:"title" bson:"title"`
 }
 
 type Problem struct {
